3f1375b8e56563cbf09dcb4bc133029eb7fc5b47
Oybek <hhoybek@gmail.com>
2021-02-25 16:13:15 +0500

    added config files

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        '542401240198-58staa66oid964i8dgdjvm5pve88tva9.apps.googleusercontent.com',
+    googleClientSecret: '7NE51VuI3yYrPbIfsNowsT58',
+    mongoURI: 'mongodb://127.0.0.1:27017/blogster',
+    cookieKey: 'cookie321',
+    redisURL: 'redis://127.0.0.1:6379'
+};
