c61afbcb62f2632a57cc815c9e2af4c0f0f5a1b2
oybek703 <hhoybek@gmail.com>
2021-04-19 20:57:55 +0500

    hide config vars

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '542401240198-58staa66oid964i8dgdjvm5pve88tva9.apps.googleusercontent.com',
-  googleClientSecret: '7NE51VuI3yYrPbIfsNowsT58',
-  mongoURI: 'mongodb://localhost:27017/blogster',
-  cookieKey: 'cookie321',
-  redisURL: 'redis://127.0.0.1:6379'
-}
